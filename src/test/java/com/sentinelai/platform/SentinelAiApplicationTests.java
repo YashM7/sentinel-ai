9922d8346c719a3861396a0e84ddae11e7803248
Yash <60492309+YashM7@users.noreply.github.com>
2026-07-29 05:37:40 -0500

    chore: testing automated GitHub Actions CI pipeline

--- src/test/java/com/sentinelai/platform/SentinelAiApplicationTests.java
+++ src/test/java/com/sentinelai/platform/SentinelAiApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class SentinelAiApplicationTests extends BasePostgresTest {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void CI_check() {
+		assertEquals(1,1);
 	}
 
 }
